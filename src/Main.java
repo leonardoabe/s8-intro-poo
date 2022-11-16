c30529f33bfc5fd77a91bb471cfc720394b05e98
abe <leo_abe@icloud.com>
2022-11-16 18:35:49 -0300

    1st-exercise - implementing exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import entities.Rectangle;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        ex1();
+    }
+
+    /**
+     * Fazer um programa para ler os valores da largura e altura de um retângulo.
+     * Em seguida, mostrar na tela o valor de sua área, perímetro e diagonal.
+     */
+    public static void ex1() {
+        System.out.println("1st exercise");
+        System.out.println("Insert rectangle width: ");
+        double width = sc.nextDouble();
+        System.out.println("Insert rectangle height: ");
+        double height = sc.nextDouble();
+        Rectangle rectangle = new Rectangle(width, height);
+
+        System.out.printf("Rectangle area: %.2f\n", rectangle.area());
+        System.out.printf("Rectangle perimeter: %.2f\n", rectangle.perimeter());
+        System.out.printf("Rectangle diagonal: %.2f\n", rectangle.diagonal());
     }
 }
